4a1cdafd23495995d89919041d12f7aac4b17f2a
JesicaValdez <jesica.valddez@gmail.com>
2023-03-12 18:12:09 -0300

    corrijo sintaxis

--- asincrono/ultimoModulo/api.js
+++ asincrono/ultimoModulo/api.js
@@ -4,6 +4,6 @@ const url = "https://pokeapi.com/api/v2/pokemon/pikachu";
 /* Llamado a la url*/
 fetch(url)
     //pasamos la respuesta a json
-    .then((res) => res.json());
+    .then((res) => res.json())
     //imprimir la respuesta ya en json
     .then((dato) => console.log(dato));
